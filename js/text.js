65d51aecc145946093daa91b953ba022bbd174de
noof <no0of9a@gmail.com>
2019-07-01 22:32:25 +0300

    add noof

---
+++ js/text.js
@@ -0,0 +1,181 @@
+
+    score1.onclick =  function name(){
+    score1.style.color = '#fbdd06';
+    window.alert("poor ")
+    };
+
+    score2.onclick =  function name(){
+    score1.style.color = '#fbdd06';
+    score2.style.color = '#fbdd06';
+    window.alert(" Below Average ")
+    };
+
+    score3.onclick =  function name(){
+    score1.style.color = '#fbdd06';
+    score2.style.color = '#fbdd06';
+    score3.style.color = '#fbdd06';
+    window.alert(" Average ")
+    };
+
+    score4.onclick =  function name(){
+    score1.style.color = '#fbdd06';
+    score2.style.color = '#fbdd06';
+    score3.style.color = '#fbdd06';
+    score4.style.color = '#fbdd06';
+    window.alert(" Above Average ")
+    };
+    score5.onclick =  function name(){
+    score1.style.color = '#fbdd06';
+    score2.style.color = '#fbdd06';
+    score3.style.color = '#fbdd06';
+    score4.style.color = '#fbdd06';
+    score5.style.color = '#fbdd06';
+    window.alert(" Excellent ")
+    };
+     
+// star2
+
+scor1.onclick =  function name(){
+    score1.style.color = '#fbdd06';
+    window.alert("poor ")
+        };
+    
+    scor2.onclick =  function name(){
+    scor1.style.color = '#fbdd06';
+    scor2.style.color = '#fbdd06';
+    window.alert(" Below Average ")
+    };
+    
+    scor3.onclick =  function name(){
+    scor1.style.color = '#fbdd06';
+    scor2.style.color = '#fbdd06';
+    scor3.style.color = '#fbdd06';
+    window.alert(" Average ")
+    };
+    
+    scor4.onclick =  function name(){
+    scor1.style.color = '#fbdd06';
+    scor2.style.color = '#fbdd06';
+    scor3.style.color = '#fbdd06';
+    scor4.style.color = '#fbdd06';
+    window.alert(" Above Average ")
+    };
+    scor5.onclick =  function name(){
+    scor1.style.color = '#fbdd06';
+    scor2.style.color = '#fbdd06';
+    scor3.style.color = '#fbdd06';
+    scor4.style.color = '#fbdd06';
+    scor5.style.color = '#fbdd06';
+    window.alert(" Excellent ")
+    };
+
+//star3
+
+scora1.onclick =  function name(){
+    score1.style.color = '#fbdd06';
+    window.alert("poor ")
+        };
+    
+    scora2.onclick =  function name(){
+    scora1.style.color = '#fbdd06';
+    scora2.style.color = '#fbdd06';
+    window.alert(" Below Average ")
+    };
+    
+    scora3.onclick =  function name(){
+    scora1.style.color = '#fbdd06';
+    scora2.style.color = '#fbdd06';
+    scora3.style.color = '#fbdd06';
+    window.alert(" Average ")
+    };
+    
+    scora4.onclick =  function name(){
+    scora1.style.color = '#fbdd06';
+    scora2.style.color = '#fbdd06';
+    scora3.style.color = '#fbdd06';
+    scora4.style.color = '#fbdd06';
+    window.alert(" Above Average ")
+    };
+    scora5.onclick =  function name(){
+    scora1.style.color = '#fbdd06';
+    scora2.style.color = '#fbdd06';
+    scora3.style.color = '#fbdd06';
+    scora4.style.color = '#fbdd06';
+    scora5.style.color = '#fbdd06';
+    window.alert(" Excellent ")
+    };
+
+
+    //star4
+
+scori1.onclick =  function name(){
+    score1.style.color = '#fbdd06';
+    window.alert("poor ")
+        };
+    
+    scori2.onclick =  function name(){
+    scori1.style.color = '#fbdd06';
+    scori2.style.color = '#fbdd06';
+    window.alert(" Below Average ")
+    };
+    
+    scori3.onclick =  function name(){
+    scori1.style.color = '#fbdd06';
+    scori2.style.color = '#fbdd06';
+    scori3.style.color = '#fbdd06';
+    window.alert(" Average ")
+    };
+    
+    scori4.onclick =  function name(){
+    scori1.style.color = '#fbdd06';
+    scori2.style.color = '#fbdd06';
+    scori3.style.color = '#fbdd06';
+    scori4.style.color = '#fbdd06';
+    window.alert(" Above Average ")
+    };
+    scori5.onclick =  function name(){
+    scori1.style.color = '#fbdd06';
+    scori2.style.color = '#fbdd06';
+    scori3.style.color = '#fbdd06';
+    scori4.style.color = '#fbdd06';
+    scori5.style.color = '#fbdd06';
+    window.alert(" Excellent ")
+    };
+
+
+
+    //star5
+
+scoro1.onclick =  function name(){
+    score1.style.color = '#fbdd06';
+    window.alert("poor ")
+        };
+    
+    scoro2.onclick =  function name(){
+    scoro1.style.color = '#fbdd06';
+    scoro2.style.color = '#fbdd06';
+    window.alert(" Below Average ")
+    };
+    
+    scoro3.onclick =  function name(){
+    scoro1.style.color = '#fbdd06';
+    scoro2.style.color = '#fbdd06';
+    scoro3.style.color = '#fbdd06';
+    window.alert(" Average ")
+    };
+    
+    scoro4.onclick =  function name(){
+    scoro1.style.color = '#fbdd06';
+    scoro2.style.color = '#fbdd06';
+    scoro3.style.color = '#fbdd06';
+    scoro4.style.color = '#fbdd06';
+    window.alert(" Above Average ")
+    };
+    scoro5.onclick =  function name(){
+    scoro1.style.color = '#fbdd06';
+    scoro2.style.color = '#fbdd06';
+    scoro3.style.color = '#fbdd06';
+    scoro4.style.color = '#fbdd06';
+    scoro5.style.color = '#fbdd06';
+    window.alert(" Excellent ")
+    };
